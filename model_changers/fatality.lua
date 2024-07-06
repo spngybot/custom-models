5899b074f66dd6abd3959e830f5caa0ada9c17bd
spngy <algermangel321@gmail.com>
2024-07-06 11:16:37 -0700

    Update fatality.lua

--- model_changers/fatality.lua
+++ model_changers/fatality.lua
@@ -1,1 +1,121 @@
 
+ffi.cdef [[
+    typedef struct{
+     void*   handle;
+     char    name[260];
+     int     load_flags;
+     int     server_count;
+     int     type;
+     int     flags;
+     float   mins[3];
+     float   maxs[3];
+     float   radius;
+     char    pad[0x1C];
+ } model_t;
+ typedef struct {void** this;} aclass;
+ typedef void*(__thiscall* get_client_entity_t)(void*, int);
+ typedef void(__thiscall* find_or_load_model_fn_t)(void*, const char*);
+ typedef const int(__thiscall* get_model_index_fn_t)(void*, const char*);
+ typedef const int(__thiscall* add_string_fn_t)(void*, bool, const char*, int, const void*);
+ typedef void*(__thiscall* find_table_t)(void*, const char*);
+ typedef void(__thiscall* full_update_t)();
+ typedef int(__thiscall* get_player_idx_t)();
+ typedef void*(__thiscall* get_client_networkable_t)(void*, int);
+ typedef void(__thiscall* pre_data_update_t)(void*, int);
+ typedef int(__thiscall* get_model_index_t)(void*, const char*);
+ typedef const model_t(__thiscall* find_or_load_model_t)(void*, const char*);
+ typedef int(__thiscall* add_string_t)(void*, bool, const char*, int, const void*);
+ typedef void(__thiscall* set_model_index_t)(void*, int);
+ typedef int(__thiscall* precache_model_t)(void*, const char*, bool);
+]]
+
+local a = ffi.cast(ffi.typeof("void***"), utils.find_interface("client.dll", "VClientEntityList003")) or error("rawientitylist is nil", 2)
+local b = ffi.cast("get_client_entity_t", a[0][3]) or error("get_client_entity is nil", 2)
+local c = ffi.cast(ffi.typeof("void***"), utils.find_interface("engine.dll", "VModelInfoClient004")) or error("model info is nil", 2)
+local d = ffi.cast("get_model_index_fn_t", c[0][2]) or error("Getmodelindex is nil", 2)
+local e = ffi.cast("find_or_load_model_fn_t", c[0][43]) or error("findmodel is nil", 2)
+local f = ffi.cast(ffi.typeof("void***"), utils.find_interface("engine.dll", "VEngineClientStringTable001")) or error("clientstring is nil", 2)
+local g = ffi.cast("find_table_t", f[0][3]) or error("find table is nil", 2)
+
+function p(pa)
+    local a_p = ffi.cast(ffi.typeof("void***"), g(f, "modelprecache"))
+    if a_p ~= nil then
+        e(c, pa)
+        local ac = ffi.cast("add_string_fn_t", a_p[0][8]) or error("ac nil", 2)
+        local acs = ac(a_p, false, pa, -1, nil)
+        if acs == -1 then 
+            print("failed")
+            return false
+        end
+    end
+    return true
+end
+
+function smi(en, i)
+    local rw = b(a, en)
+    if rw then
+        local gc = ffi.cast(ffi.typeof("void***"), rw)
+        local se = ffi.cast("set_model_index_t", gc[0][75])
+        if se == nil then
+            error("smi is nil")
+        end
+        se(gc, i)
+    end
+end
+
+function cm(ent, md)
+    if md:len() > 5 then
+        if p(md) == false then
+            error("invalid model", 2)
+        end
+        local i = d(c, md)
+        if i == -1 then
+            return
+        end
+        smi(ent, i)
+    end
+end
+
+-------------------------------------EDIT THAT ONLY------------------------------------------
+
+local path = {
+    {name = "T_artic", path = "models/player/custom_player/eminem/css/t_arctic.mdl",},
+    {name = "Lisa", path = "models/player/custom_player/kuristaja/cso2/lisa/lisa.mdl"},
+    {name = "Ballas1", path = "models/player/custom_player/eminem/gta_sa/ballas1.mdl"},
+    {name = "Tommi", path = "models/player/custom_player/nf/gta/tommi.mdl",},
+    {name = "neverlose", path = "models/player/custom_player/frnchise9812/neverlosee.mdl"},
+    {name = "gamesense", path = "models/player/custom_player/frnchise9812/gamesensee.mdl"},
+    {name = "eminem", path = "models/player/custom_player/eminem/gta_sa/wuzimu.mdl"},
+    {name = "BMYBAR", path = "models/player/custom_player/eminem/gta_sa/bmybar.mdl"},
+    {name = "Fam1", path = "models/player/custom_player/eminem/gta_sa/fam1.mdl"},
+    {name = "SOMYST", path = "models/player/custom_player/eminem/gta_sa/somyst.mdl"},
+    {name = "SWMOTR5 Duplicate", path = "models/player/custom_player/eminem/gta_sa/swmotr5.mdl"},
+    {name = "VWFYPRO", path = "models/player/custom_player/eminem/gta_sa/vwfypro.mdl"},
+    {name = "GTA Crip", path = "models/player/custom_player/z-piks.ru/gta_crip.mdl"},
+}
+
+local menu = {
+    en = gui.add_checkbox("uidmodel", "lua>tab b"),
+    path = gui.add_combo("Player Model Changer", "lua>tab b", {
+        path[1].name, path[2].name, path[3].name, path[4].name, path[5].name, 
+        path[6].name, path[7].name, path[8].name, path[9].name, path[10].name,
+        path[11].name, path[12].name, path[13].name,
+    }),
+
+}
+
+
+-------------------------------------EDIT THAT ONLY------------------------------------------
+
+function on_frame_stage_notify(stage, pre_original)
+    if stage == csgo.frame_render_start then
+        local player = entities.get_entity(engine.get_local_player())
+        if player == nil then return end
+        if player:is_alive() then
+            if menu.en:get_bool() then
+                local selected_path = path[menu.path:get_int() + 1].path
+                cm(player:get_index(), selected_path)
+            end
+        end
+    end
+end
